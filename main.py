f3d545e737f0ef79ccace56b66b5b3ba31f82fa9
pug stonks <thatonepuggo@gmail.com>
2024-02-19 21:28:40 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import json
+import sys
+from colorsys import rgb_to_hsv
+from websockets.sync.client import connect
+
+CIRCLE_ID = 1764
+CIRCLE_TYPE = [32, 5]
+DRAW_SCALE = 1
+
+def hsvify(r, g, b):
+    h, s, v = rgb_to_hsv(r / 255, g / 255, b / 255)
+    hsv_string = f'{h * 360:.2f}a{s:.2f}a{v:.2f}a0a0'
+    return hsv_string
+
+def gddata(dict: dict):
+    lis = []
+    for key, val in dict.items():
+        lis.append(f"{key},{val}")
+    return ','.join(lis)
+
+if len(sys.argv) <= 1:
+    print(f"usage: main.py <json file>")
+    exit(1)
+
+with open(sys.argv[1], "r") as f:
+    geo = json.load(f)
+    if geo.get("shapes"):
+        geo = geo["shapes"]
+
+send = {
+    "action": "ADD"
+}
+objects = []
+width = geo[0]["data"][0]
+height = geo[0]["data"][1]
+for index, item in enumerate(geo):
+    x = item["data"][0]
+    y = item["data"][1]
+    color = item["color"]
+    if item["type"] not in CIRCLE_TYPE:
+        print("not a circle, skipping...")
+        continue
+    (r, g, b, *_) = color
+    hsv = hsvify(r, g, b)
+    
+    gd_data = gddata({
+        "1": CIRCLE_ID, # object
+        "2": x / DRAW_SCALE, # x
+        "3": height - (y / DRAW_SCALE) - 1, # y
+        "25": index, # z order
+        "32": item["data"][2] / DRAW_SCALE / 4, # scale
+        "21": 10, # color channel
+        "22": 10, # color channel
+        "41": 1, # color checked
+        "42": 1,
+        "43": hsv, # color
+        "44": hsv,
+    })
+    
+    objects.append(gd_data)
+    print(gd_data)
+    
+
+send["objects"] = ";".join(objects)
+
+send["objects"] += ";"
+
+#print(send["objects"])
+
+with connect("ws://localhost:1313") as websocket:
+    websocket.send(json.dumps(send))
+    message = websocket.recv()
+    print(f"Received: {message}")
---
+++ readme.md
@@ -0,0 +1,17 @@
+# shegeometrizeonmygd
+adds an image to gd from geometrize JSONs.
+
+## how to run
+- go to [geometrize](https://www.samcodes.co.uk/project/geometrize-haxe-web/).
+- click `open image` and add an image.
+- click `settings` (above the canvas).
+- you can tweak other options, but the main thing you want to do is to **disable** all other shapes, and enable circles only.
+- set a max shapes cap. anything over 1000 is usually detailed enough but it depends on your image.
+- click `run`.
+- once it is done, click `save json`. you can use any folder, but it's recommended to save the file 
+somewhere in this directory so it's easier to run.
+- get [python](https://www.python.org/downloads/).
+- get pip by running `pip -m ensurepip --upgrade` in a terminal.
+- restart the terminal.
+- run `pip install -r requirements.txt` in this folder.
+- run `python main.py </path/to/your/json>`.
